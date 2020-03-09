bef6a9ba2111f0fd2b573f045b38d5f15dfc9b94
Mariusz Białecki <mariusz.bialecki@onet.pl>
2020-03-09 18:58:27 +0100

    add render

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
 const express = require('express');
 const path = require('path');
-const app = express();
+const hbs = require('express-handlebars');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
-});
+const app = express();
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use('/user/', (req, res, next) => {
-   res.show('forbidden.html');
+   res.render('forbidden', { layout: false });
 });
 
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', (req, res) => {
-  res.show('home.html');
+  res.render('home', { layout: false });
 });
 
 app.get('/home', (req, res) => {
-  res.show('home.html');
+  res.render('home', { layout: false });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false });
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.use((req, res) => {
